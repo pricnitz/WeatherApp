4f272a50094e933d0edd99db03416cf903cbd8ed
Nitesh Ahirwar <anitesh123.na@gmail.com>
2024-02-17 22:14:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+ const apiKey = "eb71c05ff7f941bec2b33990e7211214";
+        const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
+
+        const searchBox = document.querySelector(".search input");
+        const searchBtn = document.querySelector(".search button");
+        // const weatherIcon = document.querySelector("weather-icon");
+
+
+        async function checkWeather(city) {
+            const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+            
+            if(response.status == 404){
+                document.querySelector(".error").style.display = "block";
+                document.querySelector(".weather").style.display = "none";
+            }
+            else{
+                var data = await response.json();            
+                document.querySelector(".city").innerHTML = data.name;
+                document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+                document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+                document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+    
+                if (data.weather[0].main == "Clouds") {
+                    document.getElementById("weatherimage").src = "images/clouds.png";
+                }
+                else if (data.weather[0].main == "Clear") {
+                    document.getElementById("weatherimage").src = "images/clear.png";
+                }
+                else if (data.weather[0].main == "Rain") {
+                    document.getElementById("weatherimage").src = "images/rain.png";
+                }
+                else if (data.weather[0].main == "Drizzle") {
+                    document.getElementById("weatherimage").src = "images/drizzle.png";
+                }
+                else if (data.weather[0].main == "Mist") {
+                    document.getElementById("weatherimage").src = "images/mist.png";
+                }
+                else if (data.weather[0].main == "Haze") {
+                    document.getElementById("weatherimage").src = "images/mist.png";
+                }
+    
+                document.querySelector(".weather").style.display ="block";
+            }
+
+           
+        }
+        searchBtn.addEventListener("click", () => {
+            checkWeather(searchBox.value)
+        }) 
